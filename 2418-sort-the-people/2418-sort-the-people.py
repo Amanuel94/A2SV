22d3b9792070a032b4c65ff54d66f059729c0287
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-01-17 18:24:25 +0300

    Time: 1318 ms (5.18%), Space: 14.3 MB (8.81%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,16 +5,34 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[str]
         """
-        
+        # bubble sort
         i = len(h)-1
+        swapped = False
         while i >= 0:
             j = 0
             while j < i:
                 if h[j] < h[j+1]:
+                    swapped = True
                     h[j], h[j+1] = h[j+1], h[j]
                     names[j], names[j+1] = names[j+1], names[j]
                 j+=1
+            if not swapped:
+                return names
             i-=1
         return names
-            
+        
+        
+        
+        # i = 0
+        # while i < len(h)-1:
+        #     j = i+1
+        #     large = j
+        #     while j < len(h):
+        #         if h[large] < h[j]:
+        #             large = j
+        #         j+=1
+        #     h[i], h[large] = h[large], h[i]
+        #     names[i], names[large] = names[large], names[i]
+        #     i+=1
+        # return names
         
