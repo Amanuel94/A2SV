35873a26b0102a3616d6bd61ddb6ce5b30ed9b7b
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-01-18 20:28:51 +0300

    Time: 78 ms (70.99%), Space: 13.6 MB (22.13%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        
+        for i, row in enumerate(board):
+            if not self.check_list(row):
+                # print(row)
+                return False
+            if not self.check_list([r[i] for r in board]):
+                # print([r[i] for r in board])
+                return False
+            if not self.check_list([board[j//3 + 3*(i//3)][j%3 + (3*i)%9] for j in range(9)]):
+                # print([board[j//3 + 3*(i//3)][j%3 + (3*i)%9] for j in range(9)])
+                return False
+        return True
+        
+    def check_list(self, row):
+        counter = []
+        for num in row:
+            if num in counter:
+                return False
+            elif num != '.':
+                counter.append(num)
+        return True
+    
