b10c6bbd4ca9870be16d9f35049a730a4c4a31ab
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-01-23 18:16:48 +0300

    Create HR-bubble-sort.py

---
+++ HR-bubble-sort.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    # Write your code here
+    i = 0
+    count = 0
+    while i < len(a):
+        j = 0
+        while j <= len(a)-2:
+            if a[j] > a[j+1]:
+                a[j],a[j+1] = a[j+1], a[j]
+                count+=1
+            j+=1
+        i+=1
+    print("Array is sorted in " + str(count) + " swaps.")
+    print("First Element: " + str(a[0]))
+    print("Last Element: " + str(a[-1]))
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
+nHR
