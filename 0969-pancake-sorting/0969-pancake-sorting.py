ad49b5a4ec65d734bfed7acf34ebbac2035dfd31
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-01-23 11:19:34 +0300

    Time: 63 ms (27.08%), Space: 13.3 MB (89.58%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,15 +1,27 @@
 class Solution(object):
     def pancakeSort(self, arr):
-        nums = sorted(arr)
-        pancake = []
-        for i in range(len(nums)):
-            if arr[-1-i] == nums[i]:
-                continue
-            else:
-                pancake.append(arr.index(nums[i]) + 1)
-                arr = list(reversed(arr[:pancake[-1]])) + arr[pancake[-1]:]
-                pancake.append(len(arr)-i)
-                arr = list(reversed(arr[:pancake[-1]])) + arr[pancake[-1]:]
-                print(arr)
-        pancake.append(len(arr))
-        return pancake
+    
+        n = len(arr) # size of array
+        i = n - 1
+
+        pan = []
+
+        while i >= 0:
+            j = 0
+            max_ = arr[0]
+            largest = 0
+            while j <= i:
+                if arr[j] > arr[largest]:
+                    largest = j
+                j+=1
+            pan.append(largest+1)
+            pan.append(i+1)
+            
+            arr = arr[i:largest:-1]+ arr[:largest+1] + arr[i+1:]
+            
+            i-=1
+        return pan
+            
+                
+    
+        
