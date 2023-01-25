05d2f58c360ef53cc16b920955487bf939dcfaf1
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-01-25 09:38:58 +0300

    Time: 211 ms (13.19%), Space: 14.7 MB (54.30%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -14,9 +14,27 @@ class Solution(object):
                 r+=1
             elif l < r:
                 l+=1
-        nums.sort()
+        # nums.sort()
+        
+        # return nums.index(101)
+        l = 0
+        r = 1
+        
+        while r < len(nums):
+            if nums[r] == 101:
+                r+=1
+            elif l < r and nums[l] != 101:
+                l+=1
+            elif l == r:
+                r+=1
+            if r< len(nums) and nums[l] == 101 and nums[r] < 101:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r+=1
         nums.append(101)
-        return nums.index(101)
+        return nums.index(101) if len(nums) -1 else 1
+            
+        
                 
         
         
