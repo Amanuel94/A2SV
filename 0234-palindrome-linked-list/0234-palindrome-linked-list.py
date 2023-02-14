ec218840d09bec47aff1a4522dec366553103bfe
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-02-14 08:30:32 +0300

    Time: 1118 ms (46.46%), Space: 85.4 MB (33.84%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        li = []
+        while head:
+            li.append(head.val)
+            head = head.next
+        return li == li[::-1]
