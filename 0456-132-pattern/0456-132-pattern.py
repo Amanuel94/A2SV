cacb56938839e6aaef4a4e69ceae89bae9a657db
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-03-02 22:37:41 +0300

    Delete 0456-132-pattern directory

--- 0456-132-pattern/0456-132-pattern.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def find132pattern(self, nums):
-        stack, candidate = [], float('-inf')
-        for num in reversed(nums):
-            if num < candidate: return True
-            while stack and stack[-1] < num:
-                candidate = stack.pop()
-            stack.append(num)
-        return False
