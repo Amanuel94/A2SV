0ab70b534051a0075a065e9ff7586039bb39eaf1
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-03-27 12:51:11 +0300

    Create find-all-duplicates-in-an-array.py

---
+++ find-all-duplicates-in-an-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+      
+        i = 0
+        while i < len(nums):
+            
+            if nums[i] - i == 1:
+                i+=1
+                
+            elif nums[nums[i] - 1] != nums[i]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+            elif nums[nums[i]-1] == nums[i]:
+                i+=1
+                
+        ans = []    
+        for i, n in enumerate(nums):
+            if n != i+1:
+                ans.append(n)
+        return ans
