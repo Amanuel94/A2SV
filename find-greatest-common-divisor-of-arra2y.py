508f1bb7715b6471a0e84eef7733283eded8a823
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-04-05 12:01:11 +0300

    Create find-greatest-common-divisor-of-arra2y.py

---
+++ find-greatest-common-divisor-of-arra2y.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+    
+        
+        a = max(nums)
+        b = min(nums)
+        
+        if b == 0:
+            
+            return a
+        
+        else:
+            return self.findGCD([a%b, b])
+        
