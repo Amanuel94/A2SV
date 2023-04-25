3af80c4d262f2c984d3316b337ae445ba6ddf9a5
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-04-25 12:02:24 +0300

    added container-with-most-water.python

---
+++ container-with-most-water.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxArea(self, height):
+        maxarea = 0
+        left = 0
+        right = len(height) - 1
+        
+        while left < right:
+            width = right - left
+            maxarea = max(maxarea, min(height[left], height[right]) * width)
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+                
+        return maxarea
