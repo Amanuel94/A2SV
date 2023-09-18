61edf736f5104bbe0455fb48b903289e78f7c569
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-09-18 10:19:10 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,14 +1,19 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         
-        ans = 0
-        d = 2
-        
-        while d*d <= n:
-            divides_n  = False
-            while n%d == 0:
-                n//=d
-                ans+=d
-                            
-            d+=1
-        return ans + n if n - 1 else ans
+        # dp[i][0] not copied i As
+        # dp[i][1] copied i As
+
+        # dp[i][0] = min_over j dp[j][1] + 1
+        # dp[i][1] = dp[i][1]+1
+
+        dp = [[float('inf'), float('inf')] for i in range(n+1)]
+        dp[1][0] = 0
+        dp[1][1] = 1
+        for i in range(1, n+1):
+            for j in range(1, i):
+                if i%j == 0:
+                    dp[i][0] = min(dp[j][1]+(i//j-1), dp[i][0])
+            dp[i][1] = dp[i][0]+1
+        print(dp)
+        return dp[n][0]
