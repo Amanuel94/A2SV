8db3152675a536d85fdde36d35e39caa209410fa
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-09-26 07:13:47 +0300

    added reducing-dishes.python

--- reducing-dishes.py
+++ reducing-dishes.py
@@ -1,21 +1,17 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
 
-
-        """
-        sort the satisfaction array and take the suffix sum twice until it reaches negative
-        """
         satisfaction.sort()
-        i = len(satisfaction)-1
-        suffix_arr = []
-        suffix_sum = 0
-        while i >= 0:
-            suffix_sum+=satisfaction[i]
-            suffix_arr.append(suffix_sum)
-            i-=1
-        pre = 0
-        for suffix in suffix_arr:
-            if suffix < 0:
-                return pre
-            pre+=suffix
-        return pre
+
+        @cache
+        def dp(pos, time):
+            if pos == len(satisfaction)-1:
+                return max(0, satisfaction[-1]*time)
+            
+            ans = 0
+            ans = max(dp(pos+1, time+1)+satisfaction[pos]*time,
+                      dp(pos+1, time), 
+                      ans)
+            return ans
+
+        return dp(0, 1)
