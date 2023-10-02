191b30bcd437b3a732afb82f1ab21a27ba584219
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-10-02 21:15:08 +0300

    added k-th-smallest-in-lexicographical-order.python

--- k-th-smallest-in-lexicographical-order.py
+++ k-th-smallest-in-lexicographical-order.py
@@ -1,56 +1,55 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
 
-        # checks the number of strings < n with pref s
-        if n == 1 and k == 1:
-            return 1
-        def search(s, n):
+        # checks the number of strings < n with prefix 'prefix'
+
+        def search(prefix, n):
         
-            r = len(s)
-            k = len(str(n))
-            m = k - r
-            if m < 0:
+            pref_len = len(prefix)
+            n_len = len(str(n))
+            len_diff = n_len - pref_len
+            if len_diff < 0:
                 return 0
-            if n == int(s):
+            if n == int(prefix):
                 return 1
-            ans = "1"*(k-r)
-            if int(s) < int(str(n)[:r]):
-                return int("1"+ans) if ans else 1
-            if int(s) == int(str(n)[:r]):
-                return int(ans)+int(str(n)[-m:])+1 if ans else int(str(n)[-m:])+1
-            if int(s) >int(str(n)[:r]):
+
+            ans = "1"*(len_diff)
+            if int(prefix) < int(str(n)[:pref_len]):
+                return int("1"+ans) 
+            if int(prefix) == int(str(n)[:pref_len]):
+                return int(ans)+int(str(n)[-len_diff:])+1 if ans else int(str(n)[-len_diff:])+1 
+            if int(prefix) >int(str(n)[:pref_len]):
                 return int(ans) if ans else 0
 
-        N = 0
+        smaller_nums = 0
         ans = ""        
         for i in range(1, 10):
             nums = search(str(i), n)
-            if N+nums > k-1:
+            if smaller_nums+nums > k-1:
                 break
-            if N+nums == k-1:
+            if smaller_nums+nums == k-1:
                 return i+1
-            N+=nums
+            smaller_nums+=nums
         ans+=str(i)
         if int(ans) >= n:
-            if N-k+1:
-                    return int(ans[:-(N-k+1)])
+            if smaller_nums-k+1:
+                    return int(ans[:-(smaller_nums-k+1)])
             return int(ans)
 
         while True:
             for i in range(10):
                 nums = search(ans + str(i), n)
                 # print(nums, ans+str(i))
-                if N+nums+1 > k-1:
+                if smaller_nums+nums+1 > k-1:
                     break
-                if N+nums+1 == k-1:
+                if smaller_nums+nums+1 == k-1:
                     return int(ans+str(i+1))
-                N+=(nums)
+                smaller_nums+=(nums)
             ans+=str(i)
-            N+=1
+            smaller_nums+=1
             if n == 10:
-                print(N, k)
+                print(smaller_nums, k)
             if int(ans) >= n:
-                # print("here", ans)
-                if N-k+1:
-                    return int(ans[:-(N-k+1)])
+                if smaller_nums-k+1:
+                    return int(ans[:-(smaller_nums-k+1)])
                 return int(ans)
