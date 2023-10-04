91bd8f77abacf70fffbf594131dc84e8549bbebd
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-10-04 09:50:56 +0300

    added course-schedule-iv.python

--- course-schedule-iv.py
+++ course-schedule-iv.py
@@ -1,28 +1,17 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        vis = set()
-        graph = defaultdict(list)
-        pre = defaultdict(set)
-        
-        for u, v in prerequisites:
-            graph[u].append(v)
 
-        def dfs(node):
-    
-            
-            vis.add(node)
-            pre[node].add(node)
-            for nbr in graph[node]:
-                if nbr not in vis:
-                    dfs(nbr)
-                pre[node]|=pre[nbr]
-
-
-        res = []
-        # print(dict(pre))
-        for node in range(numCourses):
-            if node not in vis:
-                dfs(node)
-        for u, v in queries:
-            res.append(v in pre[u])
-        return res
+        A = [[inf]*numCourses for _ in range(numCourses)]
+        for pre, course in  prerequisites:
+            A[pre][course] =1
+        for i in range(numCourses):
+            A[i][i] = 0
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    A[i][j] = min(A[i][j], A[i][k] + A[k][j])
+        answer = []
+        for p, c in queries:
+            # print(A)
+            answer.append(A[p][c] <  inf)
+        return answer
