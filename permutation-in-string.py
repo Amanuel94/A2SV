41a91be64cb12ce8fc4c6fbba23d74c887883a56
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-10-09 16:53:05 +0300

    added permutation-in-string.python

--- permutation-in-string.py
+++ permutation-in-string.py
@@ -1,12 +1,5 @@
-from collections import Counter
-
-class Solution(object):
+class Solution:
     def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
         if len(s1) > len(s2):
             return False
         
