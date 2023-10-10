5ba7a04df948bf17cc92693184b72b6a258f7982
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-10-10 13:27:56 +0300

    added k-th-smallest-in-lexicographical-order.python

--- k-th-smallest-in-lexicographical-order.py
+++ k-th-smallest-in-lexicographical-order.py
@@ -1,55 +1,66 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
 
-        # checks the number of strings < n with prefix 'prefix'
-
-        def search(prefix, n):
-        
+        # Helper function to count numbers with specific prefix
+        def countNumbersWithPrefix(prefix, n):
             pref_len = len(prefix)
             n_len = len(str(n))
             len_diff = n_len - pref_len
+
             if len_diff < 0:
                 return 0
+
+            # If prefix equals n, return 1
             if n == int(prefix):
                 return 1
 
-            ans = "1"*(len_diff)
+            # Calculate the count of numbers with the given prefix
+            count = "1" * (len_diff)
             if int(prefix) < int(str(n)[:pref_len]):
-                return int("1"+ans) 
-            if int(prefix) == int(str(n)[:pref_len]):
-                return int(ans)+int(str(n)[-len_diff:])+1 if ans else int(str(n)[-len_diff:])+1 
-            if int(prefix) >int(str(n)[:pref_len]):
-                return int(ans) if ans else 0
+                return int("1" + count)
+            elif int(prefix) == int(str(n)[:pref_len]):
+                return int(count) + int(str(n)[-len_diff:]) + 1 if count else int(str(n)[-len_diff:]) + 1
+            else:
+                return int(count) if count else 0
 
         smaller_nums = 0
-        ans = ""        
+        ans = ""
+
+        # Loop through prefixes (1 to 9) to find the kth number
         for i in range(1, 10):
-            nums = search(str(i), n)
-            if smaller_nums+nums > k-1:
+            nums = countNumbersWithPrefix(str(i), n)
+            # If current prefix contains the kth number
+            if smaller_nums + nums > k - 1:
                 break
-            if smaller_nums+nums == k-1:
-                return i+1
-            smaller_nums+=nums
-        ans+=str(i)
+            # If kth number is in the next prefix
+            if smaller_nums + nums == k - 1:
+                return i + 1
+            smaller_nums += nums
+
+        ans += str(i)
+
+        # Handle edge case when kth number exceeds n
         if int(ans) >= n:
-            if smaller_nums-k+1:
-                    return int(ans[:-(smaller_nums-k+1)])
+            if smaller_nums - k + 1:
+                return int(ans[:-(smaller_nums - k + 1)])
             return int(ans)
 
+        # Loop through prefixes and find the kth number
         while True:
             for i in range(10):
-                nums = search(ans + str(i), n)
-                # print(nums, ans+str(i))
-                if smaller_nums+nums+1 > k-1:
+                nums = countNumbersWithPrefix(ans + str(i), n)
+                # If current prefix contains the kth number
+                if smaller_nums + nums + 1 > k - 1:
                     break
-                if smaller_nums+nums+1 == k-1:
-                    return int(ans+str(i+1))
-                smaller_nums+=(nums)
-            ans+=str(i)
-            smaller_nums+=1
-            if n == 10:
-                print(smaller_nums, k)
+                # If kth number is in the next prefix
+                if smaller_nums + nums + 1 == k - 1:
+                    return int(ans + str(i + 1))
+                smaller_nums += nums
+            ans += str(i)
+            smaller_nums += 1
+
+            # Handle edge case when kth number exceeds n
             if int(ans) >= n:
-                if smaller_nums-k+1:
-                    return int(ans[:-(smaller_nums-k+1)])
+                if smaller_nums - k + 1:
+                    return int(ans[:-(smaller_nums - k + 1)])
                 return int(ans)
