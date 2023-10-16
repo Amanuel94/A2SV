3fef31888c8f02343922da6a8da012a827b71dc0
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-10-16 09:23:50 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -8,8 +8,14 @@ class Solution:
         #         memo[(m,n)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
         # return memo[(m, n)]
 
-        dp = [[1]*n for i in range(m)]
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i][j-1]+ dp[i-1][j]
-        return dp[m-1][n-1]
+        # dp = [[0]*n for i in range(m)]
+        # dp[0][0]=1
+        # for i in range(m):
+        #     for j in range(n):
+        #         if j < n-1:
+        #             dp[i][j+1] += dp[i][j]
+        #         if i < m-1:
+        #             dp[i+1][j] += dp[i][j]
+        # return dp[m-1][n-1]
+
+        return math.comb(n+m-2, n-1)
